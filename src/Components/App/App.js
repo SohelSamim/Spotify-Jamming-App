13d8b979e69cd8bfc8c05a6464afa46aaa0a41df
Sohel Samimi <47162719+SohelSamim@users.noreply.github.com>
2022-08-28 22:57:59 +0200

    ok
    new updates

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,15 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      SearchResults: [{name: 'name1', album1: 'album1', artist1: 'artis1', id: 1 },
+        {name: 'name2', album: 'album2', artist: 'artis2', id: 2 },
+        {name: 'name3', album: 'album3', artist: 'artis3', id: 3 }]
+    }
+
+  }
   render() {
     return (
       <div>
@@ -12,7 +21,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
     <div className="App-playlist">
-            <SearchResults />
+            <SearchResults SearchResults={this.state.SearchResults} />
             <Playlist />
     </div>
   </div>
