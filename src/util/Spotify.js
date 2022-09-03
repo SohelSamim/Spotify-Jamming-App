097c05508f22b9dcda34199915ec43d6e040cdeb
Sohel Samimi <47162719+SohelSamim@users.noreply.github.com>
2022-09-03 22:57:03 +0200

    Implement Spotify Search Request
    Implement Spotify Search Request

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,14 +1,38 @@
-
+const clientId = '083ea5b30084491d897c9afffe95c080';
+const redirectUri = 'http://localhost:3000/';
 
 let accessToken;
+
 const Spotify = {
-  getAccessToken({
+  getAccessToken(){
     if (accessToken) {
       return accessToken;
     }
-  })
+
+    const accessTokenMatch = window.location.href.match(/accessToken=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessToken && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
 
 
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('access Token', null, '/')
+      return (accessToken);
+    } else {
+      const accessUrl =
+        `https://accounts.spotify.com/authorize?client_id=${clientId}
+        &response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      window.location = accessUrl;
+    }
+
+  },
+
+  search(term) {
+    const accessToken = Spotify.getAccessToken();
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`);
+  }
 
 }
 
